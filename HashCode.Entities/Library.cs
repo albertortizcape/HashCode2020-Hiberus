4a546351e6675e5231d303436003d5b118ed0f4b
aasanchez <aasanchez@hiberus.com>
2020-02-20 20:25:28 +0100

    ordenamiento burbuja

--- HashCode.Entities/Library.cs
+++ HashCode.Entities/Library.cs
@@ -19,8 +19,22 @@ namespace HashCode.Entities
         public List<Book> OrderedBooksByScore()
         {
             // TODO: Aplicar algoritmo de ordenación sobre los libros de la librería
-
-            return null;
+            List<Book> aux = new List<Book>();
+            aux = Libros;
+            Book libroTemporal = new Book();
+            int t = Libros.Count();
+            for (int i = 1; i < t; i++)
+                for (int j = t - 1; j >= i; j--)
+                {
+                    if (aux.ElementAt(j).Puntuacion < aux.ElementAt(j-1).Puntuacion)
+                    {
+                        libroTemporal = aux.ElementAt(j);
+                        aux.Insert(j, aux.ElementAt(j - 1));
+                        aux.Insert(j-1, libroTemporal);
+                    }
+                }
+            Libros = aux;
+            return Libros;
         }
     }
 }
