fa9830fd646e392197955b3986bdcbaa1bf2f4ac
aortiz <aortiz@hiberus.com>
2020-02-20 19:44:22 +0100

    Primera aproximación

---
+++ HashCode.Entities/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode.Entities
+{
+    public class Library
+    {
+        public List<Book> Libros { get; set; }
+
+        // Número de días que cuesta realizar el registro de todos los libros
+        public int DiasTotalesRegistro { get; set; }
+
+        // Número de libros que puede enviar la librería por día
+        public int NumeroLibrosEnvioDia { get; set; }
+
+        public List<Book> OrderedBooksByScore()
+        {
+            // TODO: Aplicar algoritmo de ordenación sobre los libros de la librería
+
+            return null;
+        }
+    }
+}
---
+++ HashCode.Entities/Resolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode.Entities
+{
+    public class Resolver
+    {
+        public int NumeroLibros { get; set; }
+        public Library[] Libraries { get; set; }
+        public int DiasEscaneo { get; set; }
+
+        public List<Book> BookList;
+
+
+        public string ReadData(string dataInput)
+        {
+
+        }
+
+        public void ReadFirstLine(string firstLine)
+        {
+            string[] dta = firstLine.Split(' ');
+            NumeroLibros = int.Parse(dta[0]);
+            int numLibraries = int.Parse(dta[1]);
+
+            Libraries = new Library[numLibraries];
+            DiasEscaneo = int.Parse(dta[2]);
+        }
+
+
+        public void ReadScoreLine(string secondLine)
+        {
+            string[] dta = secondLine.Split(' ');
+            for (int i = 0; 0 < dta.Length; i++)
+            {
+                var book = new Book();
+                book.Id = i;
+                book.Puntuacion = int.Parse(dta[i]);
+            }
+        }
+    }
+}
