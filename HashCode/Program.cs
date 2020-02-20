4357d6a91b5f6a504a584383225676fc8f805f3f
Guillermo Lobera <globera@hiberus.com>
2020-02-20 20:00:00 +0100

    dsds

--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HashCode.Entities;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,13 +12,59 @@ namespace HashCode
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader("");
-            string linea;
-            int numLinea=0;
-            while((linea=sr.ReadLine()) != null)
+            string[] lines = System.IO.File.ReadAllLines(@"C:\ruta\archivo.txt");
+            System.Console.WriteLine("Contenido del archivo = ");
+
+
+            //6 2 7
+            //  1 2 3 6 5 4
+
+            Resolver resolver = new Resolver();
+
+            int cont = 0;
+            foreach (string line in lines)
             {
-                numLinea++;
+
+                cont++;
+
+                // primera linea
+                if (cont == 1)
+                {
+                    resolver.ReadFirstLine(line);
+                }
+                // seguna linea 
+                if (cont == 2)
+                {
+                    resolver.ReadScoreLine(line);
+                }
+                // tercera linea 
+                if (cont == 3)
+                {
+                    resolver.ReadScoreLine(line);
+                }
+                // cuarta 
+                if (cont == 4)
+                {
+                    resolver.ReadScoreLine(line);
+                }
+
+                if (cont > 4)
+                {
+                    cont = 2;
+                }
+
+
+
+
+
+
+
+
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
             }
         }
+
+        
     }
 }
