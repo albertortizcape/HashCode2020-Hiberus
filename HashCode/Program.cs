9c77d449a6c8c8cdb019ae8345087b83f782a4de
Guillermo Lobera <globera@hiberus.com>
2020-02-20 19:47:20 +0100

    inicio streamreader

--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace HashCode
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("");
+            string linea;
+            int numLinea=0;
+            while((linea=sr.ReadLine()) != null)
+            {
+                numLinea++;
+            }
         }
     }
 }
